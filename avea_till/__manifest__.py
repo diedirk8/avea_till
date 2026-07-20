2b7a841a5dd38ba19a83a41b8b43d2e14e1f32ab
Dirk Visagie <dirk@aveamarketing.co.za>
2026-07-20 07:16:23 +0000

    Initial Odoo module structure

---
+++ avea_till/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Avea Till",
+    "summary": "Professional till ledger and cash management for Odoo POS.",
+    "description": """
+Avea Till
+
+A professional till ledger for Odoo Point of Sale.
+
+Features:
+- Live till balance
+- Cash ledger
+- Cash In / Cash Out
+- Dashboard
+""",
+    "version": "19.0.1.0.0",
+    "author": "Avea Software",
+    "website": "https://github.com/diedirk8/avea_till",
+    "license": "LGPL-3",
+    "category": "Point of Sale",
+    "depends": [
+        "point_of_sale",
+        "account",
+    ],
+    "data": [
+    ],
+    "assets": {
+    },
+    "installable": True,
+    "application": True,
+}
